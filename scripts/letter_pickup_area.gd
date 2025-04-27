05d5795a274ccbded0d48da0f4d543c394f10548
dylana62 <47896460+dylana62@users.noreply.github.com>
2025-04-27 19:35:31 -0400

    Added letter pickup area
    Entering area with letter sends player back to starting room

--- scripts/letter_pickup_area.gd
+++ scripts/letter_pickup_area.gd
@@ -2,23 +2,12 @@ extends Area2D
 
 @onready var letter_sprite = $"../Area2D/Letter"
 @onready var player = $"../player"
+@onready var SceneTransitionAnimation = $"../SceneTransitionAnimation/AnimationPlayer"
 
-
-
-		
-#func _process(_delta):
-	#if player_inside and Input.is_action_just_pressed("interact"):
-		##letter_sprite.follow_player()
-		#following = true
-		#label.hide()
-		#interact_tutorial_seen=true;
-		#$PaperSound.play()
-		#DialogueManager.show_dialogue_balloon(resource, "start")
-		#
-	#if following:
-		#position.x = player.position.x - 270
-		#position.y = player.position.y + 50
-
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	SceneTransitionAnimation.get_parent().get_node("ColorRect").color.a = 255
+	SceneTransitionAnimation.play("fade_out")
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player" and player.holding_letter == true:  #making sure it detects correct object
@@ -26,3 +15,8 @@ func _on_body_entered(body: Node2D) -> void:
 		print("collected")
 		letter_sprite.hide_letter()
 		player.holding_letter = false
+		
+		# Change scenes back to starting room
+		SceneTransitionAnimation.play("fade_in")
+		await get_tree().create_timer(1.5).timeout
+		get_tree().change_scene_to_file("res://scenes/game_level.tscn")
